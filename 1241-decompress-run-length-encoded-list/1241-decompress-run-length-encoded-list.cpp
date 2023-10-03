fe0aa4f8f9f1b4452bcf8a2867b902326e83f22e
Ahmed Tarek <86552169+Mido191020@users.noreply.github.com>
2023-10-03 10:27:05 +0300

    Time: 8 ms (12.96%), Space: 10.3 MB (16.38%) - LeetHub

--- 1241-decompress-run-length-encoded-list/1241-decompress-run-length-encoded-list.cpp
+++ 1241-decompress-run-length-encoded-list/1241-decompress-run-length-encoded-list.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
    vector<int> decompressRLElist(std::vector<int>& nums) {
-    vector<std::pair<int, int>> pairs;
-    for (int i = 0; i < nums.size(); i += 2) {
-        pairs.emplace_back(nums[i], nums[i + 1]);
-    }
-
-    std::vector<int> result;
-    for (const auto& pair : pairs) {
-        int freq = pair.first;
-        int val = pair.second;
-        // Append 'freq' number of 'val' to the result vector.
-        for (int j = 0; j < freq; ++j) {
-            result.push_back(val);
-        }
-    }
-    return result;
+       int size=nums.size();
+   vector<std::pair<int,int>>v;
+   for(int i=0;i<size;i+=2){
+       v.emplace_back(nums[i],nums[i+1]);
+   }
+    std::vector<int> res;
+   for(const auto& it:v){
+        int fre = it.first;
+       int val=it.second;
+       for(int j=0;j<fre;++j){
+           res.emplace_back(val);
+       }
+   }
+   return res;
 }
 };
