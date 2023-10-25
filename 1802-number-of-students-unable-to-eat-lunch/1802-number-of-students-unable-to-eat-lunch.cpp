57595eb027f43c893b5b7c1fab201c395f92f158
Ahmed Tarek <86552169+Mido191020@users.noreply.github.com>
2023-10-26 01:13:00 +0300

    Time: 0 ms (100%), Space: 9.1 MB (49.41%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
-    int  countStudents(vector<int>& A, vector<int>& B) {
-     int count[] = {0, 0}, n = A.size(), k;
-        for (int a : A)
-            count[a]++;
-        for (k = 0; k < n && count[B[k]] > 0; ++k)
-            count[B[k]]--;
-        return n - k;
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int count[2] = {0}; // Count of students preferring square (count[1]) and circular (count[0]) sandwiches
+
+        for (int student : students) {
+            count[student]++; // Count the students' preferences
+        }
+
+        for (int sandwich : sandwiches) {
+            if (count[sandwich] > 0) {
+                count[sandwich]--; // If there is a sandwich of current type, a student will eat it
+            } else {
+                // No students left who prefer this type of sandwich
+                return count[0] + count[1]; // Return the sum of remaining preferences
+            }
+        }
+
+        return 0; // All students have eaten, return 0
     }
 };
